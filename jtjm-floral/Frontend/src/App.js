dcf37d61969ae7f168f933e04f825b74882e1fbd
Jacob kaufenberg <Jacob.kaufenberg@icloud.com>
2024-04-10 20:44:50 -0500

    connected the pages to the components

--- jtjm-floral/Frontend/src/App.js
+++ jtjm-floral/Frontend/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Index from './component/index';
+import Shop from './component/shop';
+import Services from './component/services';
+import Wedding from './component/wedding';
+import About from './component/about';
+import Contact from './component/contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact component={Index} />
+          <Route path="/about" component={About} />
+          <Route path="/services" component={Services} />
+          <Route path="/shop" component={Shop} />
+          <Route path="/wedding" component={Wedding} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
